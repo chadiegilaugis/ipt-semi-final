d75ea6b2fe486138a6795abb2e9ba23bfd954637
unknown <chadiegilaugis@gmail.com>
2021-11-22 00:05:55 +0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,14 @@
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('js/bootstrap.min.js') }}">
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
+        {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> --}}
+        <style type="text/css">
+            .my-active span{
+                background-color: #5cb85c !important;
+                color: white !important;
+                border-color: #5cb85c !important;
+            }
+        </style>
     </head>
     <body>
 
